9303d5917828f77abe938cd78cfb4e28d475448e
nathanvpham <43254326+nathanvpham@users.noreply.github.com>
2021-11-17 20:00:52 -0800

    added more sql queries

--- app/database.js
+++ app/database.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     database: 'courseenrollment',
     user: 'root',
-    password: ''
+    password: 'asdf1234'
 });
 
 //get all students
@@ -104,14 +104,70 @@ app.get('/shoppingcart/students/:id', function(req, res){
 });
 
 // DELETE - remove course from shopping cart (based on student id)
+app.delete('/shoppingcart/:section_ID/:student_ID', function(req, res){
+    let sql = "DELETE FROM shoppingcart WHERE section_ID = ? AND student_ID = ?";
+    connection.query(sql, [req.params.section_ID, req.params.student_ID], function(err, results){
+        if (err) throw err;
+        res.send(results);
+    });
+});
 
 // DELETE - drop courses from course enrollment (based on student id)
+app.delete('/courseenrollment/:section_ID/:student_ID', function(req, res){
+    let sql = "DELETE FROM courseenrollment WHERE section_ID = ? AND student_ID = ?";
+    connection.query(sql, [req.params.section_ID, req.params.student_ID], function(err, results){
+        if (err) throw err;
+        res.send(results);
+    });
+});
 
-// SELECT - based off course name AND course number (search for classes)
+// SELECT - based off course name AND/OR course number (search for classes)
+// SEARCH for courseID with courseName IN course-> SEARCH for sectionName with courseID IN courseSection -> added on page
+app.get('/course/:courseName', function(req, res){
+    let sql = "SELECT * FROM course WHERE courseName = ?";
+    connection.query(sql, [decodeURI(req.params.courseName)], function(err, results){
+        if (err) throw err;
+        res.send(results);
+    });
+});
 
-// INSERT - courses into shopping cart, adding from search results (based on student id)
+app.get('/coursesection/:id', function(req, res){
+    let sql = "SELECT * FROM coursesection WHERE course_ID = ?";
+    connection.query(sql, [req.params.id], function(err, results){
+        if (err) throw err;
+        res.send(results);
+    });
+});
 
-// INSERT - courses into course enrollment (based on student id)
+// SEARCH for sectionName with sectionNumber IN courseSection -> added on page
+app.get('/coursesection/name/:id', function(req, res){
+    let sql = "SELECT * FROM coursesection WHERE sectionNumber = ?";
+    connection.query(sql, [req.params.id], function(err, results){
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
+
+// INSERT - courses into shopping cart, adding from search results (based on student id) - not working
+app.post("create/shoppingcart/:section_ID/:student_ID/:unitsEnrolled/:unitsWaitlisted", (req, res) =>{
+    let sql = "INSERT INTO shoppingcart VALUES (?, ?, ?, ?)";
+    connection.query(sql, [req.params.section_ID, req.params.student_ID, req.params.unitsEnrolled, req.params.unitsWaitlisted],function(err, results){
+        if (err) throw err;
+        res.send(results);
+    });
+})
+
+
+// INSERT - courses into course enrollment (based on student id) - not working
+app.post("create/courseenrollment/:section_ID/:student_ID/:grade", (req, res) =>{
+    let sql = "INSERT INTO courseenrollment VALUES (?, ?, ?)";
+    connection.query(sql, [req.body.section_ID, req.body.student_ID, req.body.grade],function(err, results){
+        if (err) throw err;
+        res.send(results);
+    });
+})
+
 
 app.listen(3000, function(){
     console.log("app listening 3000");
---
+++ app/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+req.params.section_ID
